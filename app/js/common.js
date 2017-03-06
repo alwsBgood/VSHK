829a1bdc4d9a76a80206ce0fbd74dffe73d02005
Dmitriy <dima.chebotaru@gmail.com>
2017-03-06 17:47:17 +0200

    Descktop

--- app/js/common.js
+++ app/js/common.js
@@ -1,9 +1,9 @@
-if (localStorage.name && localStorage.email && localStorage.phone)  {
-  // запись сохраненных данных сразу в поля, если надо
-  $('input[name="name"]').val(localStorage.name);
-  $('input[type="email"]').val(localStorage.email);
-  $('input[type="tel"]').val(localStorage.phone);
-}
+// if (localStorage.name && localStorage.email && localStorage.phone)  {
+//   // запись сохраненных данных сразу в поля, если надо
+//   $('input[name="name"]').val(localStorage.name);
+//   $('input[type="email"]').val(localStorage.email);
+//   $('input[type="tel"]').val(localStorage.phone);
+// }
 
 $(function() {
   $("[name=send]").click(function (e) {
@@ -24,9 +24,9 @@ $(function() {
    var goal = btn.closest('form').find('[name=goal]').val();
    var alertImage = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 286.1 286.1"><path d="M143 0C64 0 0 64 0 143c0 79 64 143 143 143 79 0 143-64 143-143C286.1 64 222 0 143 0zM143 259.2c-64.2 0-116.2-52-116.2-116.2S78.8 26.8 143 26.8s116.2 52 116.2 116.2S207.2 259.2 143 259.2zM143 62.7c-10.2 0-18 5.3-18 14v79.2c0 8.6 7.8 14 18 14 10 0 18-5.6 18-14V76.7C161 68.3 153 62.7 143 62.7zM143 187.7c-9.8 0-17.9 8-17.9 17.9 0 9.8 8 17.8 17.9 17.8s17.8-8 17.8-17.8C160.9 195.7 152.9 187.7 143 187.7z" fill="#E2574C"/></svg>';
 
-   localStorage.name = form.find('input[name="name"]').val();
-   localStorage.email = form.find('input[type="email"]').val();
-   localStorage.phone = form.find('input[type="tel"]').val();
+   // localStorage.name = form.find('input[name="name"]').val();
+   // localStorage.email = form.find('input[type="email"]').val();
+   // localStorage.phone = form.find('input[type="tel"]').val();
 
 
    $(ref).each(function() {
@@ -123,7 +123,7 @@ jQuery(function($){
 
 $(function() {
   $(".youtube").each(function() {
-    $(this).css('background-image', 'url(http://i.ytimg.com/vi/' + this.id + '/sddefault.jpg)');
+    // $(this).css('background-image', 'url(http://i.ytimg.com/vi/' + this.id + '/sddefault.jpg)');
 
     $(this).append($('<div/>', {'class': 'play'}));
 
@@ -160,8 +160,8 @@ $('.md-overlay').click(function() {
 $(document).ready(function() {
   $('.slider').slick({
     slidesToShow: 1,
-    dots: false,
-    arrows: true,
+    dots: true,
+    arrows: false,
     fade: true,
     slidesToScroll: 1,
     autoplay: false,
@@ -173,12 +173,41 @@ $(document).ready(function() {
   $('.slider_testimonial').slick({
     slidesToShow: 1,
     dots: false,
+    arrows: false,
+    slidesToScroll: 1,
+    autoplay: false,
+    adaptiveHeight: true,
+    asNavFor: '.slider_control'
+  });
+});
+
+
+$(document).ready(function() {
+  $('.slider_control').slick({
+    slidesToShow: 1,
+    dots: true,
     arrows: true,
     slidesToScroll: 1,
     autoplay: false,
-    adaptiveHeight: true
+    fade: true,
+    adaptiveHeight: true,
+    asNavFor: '.slider_testimonial'
   });
 });
+
+
+//  CURSOR ON SLIDER HOVER FUNCTION
+
+$(document).ready(function() {
+  jQuery(".slider_testimonial").mousedown(function() {
+    jQuery(this).removeClass("touch_mode_grab")
+    .addClass("touch_mode_grabbing");
+  }).mouseup(function() {
+    jQuery(this).removeClass("touch_mode_grabbing")
+    .addClass("touch_mode_grab");
+  })
+});
+
 
 // Menu
 
