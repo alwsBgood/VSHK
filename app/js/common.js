27167ec4c91b6a740f52ab1bda3e5c39f5e3228f
Dmitriy <dima.chebotaru@gmail.com>
2017-03-06 21:09:24 +0200

    adaptive

--- app/js/common.js
+++ app/js/common.js
@@ -1,9 +1,3 @@
-// if (localStorage.name && localStorage.email && localStorage.phone)  {
-//   // запись сохраненных данных сразу в поля, если надо
-//   $('input[name="name"]').val(localStorage.name);
-//   $('input[type="email"]').val(localStorage.email);
-//   $('input[type="tel"]').val(localStorage.phone);
-// }
 
 $(function() {
   $("[name=send]").click(function (e) {
@@ -24,9 +18,6 @@ $(function() {
    var goal = btn.closest('form').find('[name=goal]').val();
    var alertImage = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 286.1 286.1"><path d="M143 0C64 0 0 64 0 143c0 79 64 143 143 143 79 0 143-64 143-143C286.1 64 222 0 143 0zM143 259.2c-64.2 0-116.2-52-116.2-116.2S78.8 26.8 143 26.8s116.2 52 116.2 116.2S207.2 259.2 143 259.2zM143 62.7c-10.2 0-18 5.3-18 14v79.2c0 8.6 7.8 14 18 14 10 0 18-5.6 18-14V76.7C161 68.3 153 62.7 143 62.7zM143 187.7c-9.8 0-17.9 8-17.9 17.9 0 9.8 8 17.8 17.9 17.8s17.8-8 17.8-17.8C160.9 195.7 152.9 187.7 143 187.7z" fill="#E2574C"/></svg>';
 
-   // localStorage.name = form.find('input[name="name"]').val();
-   // localStorage.email = form.find('input[type="email"]').val();
-   // localStorage.phone = form.find('input[type="tel"]').val();
 
 
    $(ref).each(function() {
@@ -114,7 +105,7 @@ $(function() {
 //  INPUT TEL MASK
 
 jQuery(function($){
- $("input[type='tel']").mask("+9 (999) 999-9999");
+ $("input[type='tel']").mask("+99 (999) 999-9999");
 });
 
 
@@ -166,6 +157,18 @@ $(document).ready(function() {
     slidesToScroll: 1,
     autoplay: false,
     adaptiveHeight: false
+  });
+});
+
+$(document).ready(function() {
+  $('.slider_mobile').slick({
+    slidesToShow: 1,
+    dots: true,
+    arrows: true,
+    fade: false,
+    slidesToScroll: 1,
+    autoplay: false,
+    adaptiveHeight: true
   });
 });
 
@@ -235,6 +238,7 @@ $(document).ready(function() {
 
     $('#menu').click(function(){
         $('#nav').toggleClass('open');
+        $('body').toggleClass('unscroll');
     });
 
     $('#nav li a').click(function(){
@@ -249,49 +253,11 @@ $(document).ready(function() {
 
 $('.more_coaches').click(function(event) {
   $('.coaches_hidden').slideToggle();
-});
-
-// Perfect Pxel
-
-$('body').each(function() {
-  var body = $(this);
-  var img_url = $(this).data('img');
-  var img = new Image();
-  img.src = img_url;
-  img.onload = function(){
-    var ppbox = '<div id="pp" style="background: url('+img_url+') no-repeat 50% 0%;top:0;width:100%;position:absolute;z-index:1000000;opacity:0.5;height:'+img.height+'px"></div>';
-    var ppbtn = '<button onclick="myOff()" id="ppbtn" style="position:fixed;top:0;right:0;z-index:1000001">ON</button>'
-    body.append(ppbox);
-    body.append(ppbtn);
-  };
-});
-function myOff() {
-  var ppbtntext = $('#ppbtn').text();
-  if (ppbtntext == 'ON') {
-    $('#ppbtn').text('OFF');
-    $('#pp').css('display', 'none');
+  $(this).toggleClass('opened');
+  if($(this).hasClass('opened')){
+    $(this).html('Скрыть')
   } else {
-    $('#ppbtn').text('ON');
-    $('#pp')        .css({
-      ' z-index' : '1000000',
-      display: 'block'
-    });
-
-  }
-}
-
-$('html').keydown(function(){
-  var ppbtntext = $('#ppbtn').text();
-  if (event.keyCode == 81) {
-    if (ppbtntext == 'ON') {
-      $('#ppbtn').text('OFF');
-      $('#pp').css('display', 'none');
-    } else {
-      $('#ppbtn').text('ON');
-      $('#pp')        .css({
-        ' z-index' : '1000000',
-        display: 'block'
-      });
-    }
+    $(this).html('Показать всех')
   }
 });
+
